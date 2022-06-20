563ef421c0924d69f367424e5392059c02459787
BigBird1427 <shanescomputer123@gmail.com>
2022-06-20 18:23:26 -0400

    ButtonForLever

--- Assets/Script/LeverEvent.cs
+++ Assets/Script/LeverEvent.cs
@@ -2,23 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 
 public class LeverEvent : MonoBehaviour
 {
     //Calls the function that would call the event OnLeverTrigger
     //When in proximity of the lever
+    public Button Button1;
 
-    public void OnCollisionStay2D(Collision2D col){
+    //public void OnTriggerEnter2D(Collider2D col){}
+    void Start(){
+        Button1.gameObject.SetActive(false);
+    }
+
+    private void Awake(){
+        Button1.onClick.AddListener(OnButtonClicked);
+    }
+
+    private void OnButtonClicked(){
+        EventController.current.LeverTrigger();
+    }
+
+    public void OnTriggerStay2D(Collider2D col){
         //Debug.Log(col.gameObject.tag);
+        Button1.gameObject.SetActive(true);
         if(col.gameObject.CompareTag("Player"))
         {
-            if(Input.GetKey(KeyCode.Return))
+            if(Input.GetKey(KeyCode.Return)){
             EventController.current.LeverTrigger();
+            }
         }
     }
 
-     public void OnCollisionExit2D(Collision2D col){
+     public void OnTriggerExit2D(Collider2D col){
         //Debug.Log(col.gameObject.tag);
+        Button1.gameObject.SetActive(false);
         if(col.gameObject.CompareTag("Player"))
         {
             EventController.current.LeverExit();
